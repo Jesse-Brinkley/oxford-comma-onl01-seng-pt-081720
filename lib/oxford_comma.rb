ce0be2f02c418cf2baa1f51a75ffa3db6079ea6a
Jesse Brinkley <jbrink28@gmail.com>
2020-08-26 20:18:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
-    return array[0]
+    array[0]
   elsif array.length == 2
-    return array.join(" and ")
+    array.join(" and ")
   elsif array.length > 2
     array[-1].insert(0, "and ")
   end
